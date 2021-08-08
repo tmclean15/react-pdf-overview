715992567737ac0d72f371b756c4674dd9202736
Taylor McLean <taylormclean15@gmail.com>
2021-08-08 15:07:43 -0400

    project scaffold

---
+++ libs/templates/src/lib/templates.tsx
@@ -0,0 +1,14 @@
+import './templates.module.css';
+
+/* eslint-disable-next-line */
+export interface TemplatesProps {}
+
+export function Templates(props: TemplatesProps) {
+  return (
+    <div>
+      <h1>Welcome to Templates!</h1>
+    </div>
+  );
+}
+
+export default Templates;
