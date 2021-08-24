caf9a530ecfd16ffafe1842e5c9eb80bbf003e41
Taylor McLean <taylormclean15@gmail.com>
2021-08-24 07:55:16 -0400

    feat(templates): add content as props to document template

--- libs/templates/src/lib/document-template.tsx
+++ libs/templates/src/lib/document-template.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import {
   Document,
   Page,
@@ -7,14 +7,75 @@ import {
   Text,
   Link,
   Canvas,
-} from '@react-pdf/renderer';
-import { Title } from './views';
+  StyleSheet,
+} from "@react-pdf/renderer";
+import { Title, Table, TableProps, Divider } from "./views";
 
-export const DocumentTemplate = (): JSX.Element => {
+const styles = StyleSheet.create({
+  document: {
+    padding: "0.3in",
+    fontSize: 10,
+  },
+  body: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    marginBottom: "0.3in",
+  },
+  bodyParagraph: {
+    flexBasis: "60%",
+    border: "solid",
+    fontSize: 10,
+  },
+  bodyImage: {
+    flexBasis: "40%",
+    margin: "0.3in",
+  },
+  linkContainer: {
+    display: "flex",
+    flexDirection: "row",
+  },
+  link: {
+    textDecoration: "underline",
+    textDecorationStyle: "double",
+  },
+});
+
+export type DocumentTemplateProps = {
+  title: string;
+  paragraph: string;
+  src: string;
+  tableData: TableProps;
+  chartImageUrl: string;
+};
+
+export const DocumentTemplate = ({
+  title,
+  paragraph,
+  src,
+  tableData,
+  chartImageUrl,
+}: DocumentTemplateProps): JSX.Element => {
   return (
     <Document>
-      <Page>
-        <Title>Very Important Report</Title>
+      <Page style={styles.document} size="A4">
+        <Title>{title}</Title>
+        <View style={styles.body}>
+          <Text style={styles.bodyParagraph}>{paragraph}</Text>
+          <Image src={src} style={styles.bodyImage} />
+        </View>
+        <View style={styles.linkContainer}>
+          <Text>Link to Report: </Text>
+          <Link
+            src="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+            style={styles.link}
+          >
+            Click Me!
+          </Link>
+        </View>
+        <Divider />
+        <Table {...tableData} />
+        <Image src={chartImageUrl} />
       </Page>
     </Document>
   );
